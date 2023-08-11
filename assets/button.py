770be7cf940692d4675b48f21b94b4766fbc674b
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-11 12:03:28 +0700

    Update button.py

--- assets/button.py
+++ assets/button.py
@@ -1,8 +1,7 @@
 from pyrogram.types import *
 from zeldris import BOT_USERNAME
 
-class BUTTON:
-    BMANAGE = InlineKeyboardMarkup(
+BMANAGE = InlineKeyboardMarkup(
                 [
                  [
                     InlineKeyboardButton(text="💁🏻‍♂Perintah Dasar", callback_data="zel_dasar"),
@@ -19,12 +18,13 @@ class BUTTON:
                     InlineKeyboardButton(text="🔙 Kembali", callback_data="zel_back"),
                  ]
                 ])
-    BBMANAGE = InlineKeyboardMarkup(
+BBMANAGE = InlineKeyboardMarkup(
+                [
                  [
                     InlineKeyboardButton(text="🔙 Kembali", callback_data="zel_manage"),
                  ]
                 ])
-    BMUSIC = InlineKeyboardMarkup(
+BMUSIC = InlineKeyboardMarkup(
                 [
                  [
                     InlineKeyboardButton(text="ᴘᴇʀɪɴᴛᴀʜ ᴀᴅᴍɪɴ", callback_data="zel_admin"),
@@ -38,13 +38,13 @@ class BUTTON:
                     InlineKeyboardButton(text="«", callback_data="zel_back"),
                  ]
                 ])
-    BBMUSIC = InlineKeyboardMarkup(
+BBMUSIC = InlineKeyboardMarkup(
                 [
                  [
                     InlineKeyboardButton(text="🔙 Kembali", callback_data="zel_music"),
                  ]
                 ])
-    BJASA = InlineKeyboardMarkup(
+BJASA = InlineKeyboardMarkup(
               [
                  [
                      InlineKeyboardButton("Admin", url="https://t.me/msdqqq")
@@ -53,38 +53,4 @@ class BUTTON:
                     InlineKeyboardButton(text="«", callback_data="zel_back"),
                  ]
               ])
-    JASA = """
-𝙅𝘼𝙎𝘼 𝘿𝙀𝙋𝙇𝙊𝙔 𝘽𝙊𝙏 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈
-
- 🤖 𝘽𝙊𝙏 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈
-      ├ ᴜsᴇʀʙᴏᴛ 20ᴋ
-      ├ ʙᴏᴛ ᴍᴜsɪᴄ 50ᴋ
-      ├ ʙᴏᴛ ᴍᴀɴᴀɢᴇ 30ᴋ
-      ├ ʙᴏᴛ ғɪʟᴇ sʜᴀʀɪɴɢ 20ᴋ
-      ├ ʙᴏᴛ ᴍᴜsɪᴄ+ᴍᴀɴᴀɢᴇ 75ᴋ
-      ├ ᴅᴇᴘʟᴏʏ ᴅɪ ᴠᴘs
-      ├ ꜰᴜʟʟ ɢᴀʀᴀɴꜱɪ 1 ʙᴜʟᴀɴ
-      └ sɪsᴛᴇᴍ ᴛᴇʀɪᴍᴀ ᴊᴀᴅɪ
-
-✉️ 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈
-      ├ ɴᴏᴋᴏs ɪᴅ ғʀᴇsʜ 5ᴋ
-      └ ɴᴏᴋᴏs ɪᴅᴄ 𝟷 50ᴋ
-
-🌐 𝙑𝙋𝙎
-      ├ 💳 2ɢʙ : 45ᴋ
-      ├ 💳 4ɢʙ : 80ᴋ
-      └ 💳 8ɢʙ : 130ᴋ
- 
-👥 𝙍𝙀𝙆𝘽𝙀𝙍 & 𝙅𝘼𝙎𝘼 𝙋𝙄𝙉𝙅𝘼𝙈 𝙌𝙍𝙄𝙎
-      ├ 𝟶-𝟸𝟶ᴋ ʙɪᴀʏᴀ 𝟷ᴋ
-      ├ 𝟸𝟶-𝟻𝟶ᴋ ʙɪᴀʏᴀ 𝟹ᴋ
-      ├  𝟻𝟶-𝟷𝟶𝟶ᴋ ʙɪᴀʏᴀ 𝟻ᴋ
-      ├ 𝟷𝟶𝟶-𝟻𝟶𝟶ᴋ ʙɪᴀʏᴀ 𝟷𝟶ᴋ
-      ├ 𝟻𝟶𝟶-𝟷Jᴛ ʙɪᴀʏᴀ 𝟹𝟶ᴋ
-      └ ʀᴇᴋʙᴇʀ & ᴊᴀsᴘɪɴ ϙʀɪs ᴏɴʟʏ ᴅᴀɴᴀ
-
-**ᴄᴀᴛᴀᴛᴀɴ:**
-1.ʜᴀʀɢᴀ ʙɪꜱᴀ ʙᴇʀᴜʙᴀʜ ꜱᴇᴛɪᴀᴘ ꜱᴀᴀᴛ
-2.ᴛᴀɴʏᴀᴋᴀɴ ꜱᴛᴏᴄᴋ ᴋᴇᴘᴀᴅᴀ ᴀᴅᴍɪɴ ꜱᴇʙᴇʟᴜᴍ ᴍᴇᴍʙᴇʟɪ
-3.ᴊɪᴋᴀ ɪɴɢɪɴ ᴍᴇᴍʙᴇʟɪ ʜᴜʙ ᴀᴅᴍɪɴ ᴅɪʙᴀᴡᴀʜ ɪɴɪ.
-"""
+    
